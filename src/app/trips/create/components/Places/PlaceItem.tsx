3f6c2f252a02e94ef982e2e6fc583acb998fb430
John Boyle <nikolasboyle@gmail.com>
2024-01-23 09:56:44 -0800

    Fix usePlaces hook bug

--- src/app/trips/create/components/Places/PlaceItem.tsx
+++ src/app/trips/create/components/Places/PlaceItem.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { usePlace } from "@/hooks/usePlace";
+import { useGetPlace } from "@/hooks/usePlaces";
 
 type Props = { placeId: string };
 
 export default function DestinationItem({ placeId }: Props) {
-  const { data: place } = usePlace(placeId);
+  const { data: place } = useGetPlace(placeId);
 
   return place ? <div>{place.displayName.text}</div> : null;
 }
